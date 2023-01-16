b53ddba1008160c1a18b09d8c3a06a53e4b490a3
VladimiraRe <vladimira_ra@mail.ru>
2023-01-16 20:25:36 +0300

    feat: add internet connection error

--- src/components/MoviesApp/MoviesApp.js
+++ src/components/MoviesApp/MoviesApp.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 import { Component } from 'react';
-import { Layout } from 'antd';
+import { Layout, Alert } from 'antd';
 
 import MoviesList from '../MoviesList';
 import './MoviesApp.css';
@@ -8,14 +8,24 @@ import './MoviesApp.css';
 export default class MoviesApp extends Component {
     state = {
         search: '',
+        isOnline: navigator.onLine,
     };
 
     render() {
-        const { search } = this.state;
+        const { search, isOnline } = this.state;
+        if (isOnline) {
+            window.addEventListener('offline', () => this.setState({ isOnline: false }));
+        } else {
+            window.addEventListener('online', () => this.setState({ isOnline: true }));
+        }
 
         return (
             <Layout className='moviesApp'>
-                <MoviesList search={search} />;
+                {isOnline ? (
+                    <MoviesList search={search} />
+                ) : (
+                    <Alert message='Oops' description='No internet connection' type='error' />
+                )}
             </Layout>
         );
     }
